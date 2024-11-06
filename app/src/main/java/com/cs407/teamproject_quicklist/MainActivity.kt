b1bf5e2f149de2c5fcacc7afd45b2ae25a37e4a9
ryanluo <ryanluo42@gmail.com>
2024-11-06 14:18:36 -0600

    Settings commit

--- app/src/main/java/com/cs407/teamproject_quicklist/MainActivity.kt
+++ app/src/main/java/com/cs407/teamproject_quicklist/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.cs407.teamproject_quicklist
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Set up toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Change the toolbar title to "Settings"
+        supportActionBar?.title = "Settings"
+        // Or if you want to remove the default title and use the TextView from the layout:
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 }
