2ab7eec3fd8bda2ca0c12b86a55925eac7f410bc
ryanluo <ryanluo42@gmail.com>
2024-12-04 20:48:32 -0600

    Settings dropdown working

--- app/src/main/java/com/cs407/teamproject_quicklist/TaskListActivity.kt
+++ app/src/main/java/com/cs407/teamproject_quicklist/TaskListActivity.kt
@@ -3,8 +3,10 @@ package com.cs407.teamproject_quicklist
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.PopupMenu
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -69,6 +71,31 @@ class TaskListActivity : AppCompatActivity() {
         searchTaskImageView.setOnClickListener {
             showSearchDialog()
         }
+
+        // Find the more options ImageView
+        val moreOptionsImage = findViewById<ImageView>(R.id.more_option_imageview)
+        // Set click listener for the more options button
+        moreOptionsImage.setOnClickListener { view ->
+            showPopupMenu(view)
+        }
+    }
+
+    private fun showPopupMenu(view: View) {
+        val popup = PopupMenu(this, view)
+        popup.menuInflater.inflate(R.menu.more_options_menu, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_settings -> {
+                    // Launch Settings Activity
+                    val intent = Intent(this, SettingsActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+        popup.show()
     }
 
     private fun enableSwipeToEdit(recyclerView: RecyclerView) {
